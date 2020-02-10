aaec09be1f296172238dc5366d0f5d39ad640165
Traci Chan <tlc668@gmail.com>
2020-02-10 08:40:10 -0500

    initial commit String Tutorial

---
+++ StringsTutorialProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringsTutorialProject {
+    class Program {
+        static void Main(string[] args) {
+
+            var st1 = new StringsTutorial();//have an instance of our class
+            //added another method...says want object plus what we created 
+            var fullname=st1.Fullname();//call our method
+            
+            Console.WriteLine($"Full name is {fullname}");
+            Console.WriteLine($"Full name2 is {st1.Fullname2()}");//dont have to store in var
+            //create another variable and pass through params
+            var denise = new StringsTutorial("Denise", "Bartick");
+            Console.WriteLine($"Denise {denise.Fullname()}");
+            Console.WriteLine($"Denise {denise.Fullname2()}");
+            }
+        }
+    }
---
+++ StringsTutorialProject/StringsTutorial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringsTutorialProject {
+    class StringsTutorial {
+        public string Firstname;//="Leia";//initalize it these are properties of the class
+        public string Lastname;//="Chan";//exists as long as our class s1 exists-these are hard coded
+
+        //special method with no return type and has name of a class is a CONSTRUCTOR
+        //aways used to give your class data 
+        public StringsTutorial() {
+            //can't pass anything into this but can give instance data
+            //inialize data
+            Firstname = "Leia";
+            Lastname = "Chan";
+            //you are allowed to have more than one constructor
+            //if valuable if had another constructor that took in parameters
+            }
+
+        //another way passing params
+        public StringsTutorial (string firstname, string lastname) {
+            //initialize data and stick in correct property
+           
+            Firstname = firstname;
+            Lastname = lastname;
+            }
+
+        public string Fullname() {
+            //var Firstname = "Traci";
+            //var Lastname = "Chan";
+
+            //var name = Firstname + " " + Lastname;
+            //another way to do it is
+            //var name = $"{Firstname} {Lastname}";
+            var name = ($"{Lastname}, {Firstname}");//want to comment out the local variables
+            return name;
+            }
+        public string Fullname2() {
+            //var Firstname = "Traci";
+            //var Lastname = "Chan";
+
+            var name = $"{Firstname} {Lastname}";
+            return name;
+        }
+
+
+        }
+    }
